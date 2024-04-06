c9358e89ad182f34558a5e0d393ce0ddb493bfe2
Viktoriia Shpak <viktoriia.ienko@gmail.com>
2024-04-06 20:01:19 +0300

    added changes

--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ formElement.addEventListener('submit', onSubmit);
 const listElement = document.querySelector('.gallery');
 const loader = document.querySelector('.wraper-loader');
 const loadMoreBtn = document.querySelector('.load-more-btn');
+const inputElement = document.querySelector('.search-input');
 let pageCounter = 1;
 let searchQuery = '';
 const per_page = 15;
@@ -53,6 +54,40 @@ async function onSubmit(event) {
       hideLoader();
     });
   hideEndOfCollectionMessage();
+  showLoader();
+  try {
+    const images = await getPhotos(searchQuery, pageCounter, per_page);
+    const totalHits = images.totalHits;
+
+    if (images.hits.length === 0) {
+      galleryElement.innerHTML = '';
+      iziToast.info({
+        title: 'Info',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topCenter',
+      });
+      hideLoadMoreBtn();
+      return;
+    } else {
+      createMarkup(images.hits);
+      inputElement.value = '';
+      showLoadMoreBtn();
+    }
+    if (per_page * pageCounter >= totalHits) {
+      hideLoadMoreBtn();
+      showEndOfCollectionMessage();
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to fetch images. Please try again later.',
+      position: 'topCenter',
+    });
+  } finally {
+    hideLoader();
+  }
 }
 
 /* Load more */
